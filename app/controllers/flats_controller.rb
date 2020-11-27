7f6233ca63c033ba3958b6a63d494b825a2f7954
Joao Baumotte <jbaumotte@gmail.com>
2020-11-27 18:06:49 -0300

    Index ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,16 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.delete
+
+    redirect_to flat_path(@flat)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Erasing current DB 🗑️"
+Flat.delete_all
+puts "Clean as a whistle 😗"
+puts "Now to repopulate it ... 🧰"
+
+4.times do
+  new_flat = Flat.create!(
+    name: Faker::Lorem.sentence,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(70..200),
+    number_of_guests: rand(1..6)
+  )
+end
+
+puts "that's it, now go code the rest 👨‍💻"
