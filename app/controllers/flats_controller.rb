667042707ced400cae40a3283dd3dfacc14d8452
Joao Baumotte <jbaumotte@gmail.com>
2020-11-28 15:16:25 -0300

    All working OK

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,10 +7,45 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+
+  end
+
   def destroy
     @flat = Flat.find(params[:id])
     @flat.delete
 
-    redirect_to flat_path(@flat)
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
